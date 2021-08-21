5eb493c1bb820088a43b7cd3e9a2758301970ef9
mike <meehm334@gmail.com>
2021-08-20 21:03:09 -0400

    Add NavHostPhysInvUploadScreen with computer name arg passed.

---
+++ app/src/main/java/com/example/pharmscan/ui/Navigation/NavHostPhysInvUploadScreen.kt
@@ -0,0 +1,188 @@
+package com.example.pharmscan.ui.Navigation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+//import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import com.example.pharmscan.ui.Screen.Screen
+import kotlinx.coroutines.launch
+
+fun NavGraphBuilder.addPhysInvUploadScreen(navController: NavController) {
+    composable(
+        route = Screen.PhysInvUploadScreen.route + "/{hostCompName}",
+        // Define argument list to pass to this composable in composable constructor
+        // arguments parameter which is a list of navArguments.
+        arguments = listOf(
+            navArgument("hostCompName") {
+                type = NavType.StringType
+                nullable = false
+            }
+        )
+    ) {
+        // Get the arguments passed to this composable by key name
+        // arg must be present
+        val argTextHostComputer = it.arguments!!.getString("hostCompName")
+        val scaffoldState = rememberScaffoldState()
+        val coroutineScope = rememberCoroutineScope()
+
+        Scaffold(
+            scaffoldState = scaffoldState,
+            //drawerShape = RoundedCornerShape(topStart = 15.dp, topEnd = 15.dp, bottomEnd = 15.dp, bottomStart = 15.dp),
+            drawerShape = MaterialTheme.shapes.large,
+            drawerContent = {
+                Text(
+                    text = "Settings",
+                    modifier = Modifier.clickable {
+                        coroutineScope.launch {
+                            scaffoldState.drawerState.close()
+                            scaffoldState.snackbarHostState.showSnackbar("Drawer Settings") }
+                    },
+                    style = MaterialTheme.typography.caption,
+                    color = MaterialTheme.colors.onBackground
+                )
+
+                Spacer(modifier = Modifier.height(height = 10.dp))
+
+                Text(
+                    text = "View Cancel",
+                    modifier = Modifier.clickable {
+                        coroutineScope.launch {
+                            scaffoldState.drawerState.close()
+                            navController.navigate(Screen.ViewCancel.route) }
+                    },
+                    style = MaterialTheme.typography.caption,
+                    color = MaterialTheme.colors.onBackground
+                )
+
+                Spacer(modifier = Modifier.height(height = 10.dp))
+
+                Text(
+                    text = "View File Name",
+                    modifier = Modifier.clickable {
+                        coroutineScope.launch {
+                            scaffoldState.drawerState.close()
+                            navController.navigate(Screen.ViewColDataFNameScreen.route)
+                        }
+                    },
+                    style = MaterialTheme.typography.caption,
+                    color = MaterialTheme.colors.onBackground
+                )
+
+                Spacer(modifier = Modifier.height(height = 10.dp))
+
+                Text(
+                    text = "About",
+                    modifier = Modifier.clickable {
+                        coroutineScope.launch {
+                            scaffoldState.drawerState.close()
+                            navController.navigate(Screen.AboutScreen.route)
+                        }
+                    },
+                    style = MaterialTheme.typography.caption,
+                    color = MaterialTheme.colors.onBackground
+                )
+            },
+            topBar = {
+                TopAppBar(
+                    title = { Text("PharmScan") },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = {
+                                coroutineScope.launch { scaffoldState.drawerState.open() }
+                            }
+                        ) {
+                            Icon(Icons.Filled.Menu, contentDescription = "Localized description")
+                        }
+                    }
+                )
+            },
+            content = {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 10.dp),
+                    verticalArrangement = Arrangement.Top
+                ){
+                    Text(
+                        text = "<-",
+                        fontSize = 40.sp,
+                        modifier = Modifier
+                            .align(alignment = Alignment.Start)
+                            .padding(start = 20.dp)
+                            .clickable {
+                                navController.popBackStack()
+                            },
+                        style = MaterialTheme.typography.h5,
+                        color = MaterialTheme.colors.onBackground
+                    )
+                    Text(
+                        text = argTextHostComputer!!,
+                        fontSize = 40.sp,
+                        modifier = Modifier
+                            .align(alignment = Alignment.CenterHorizontally)
+                            .padding(start = 10.dp),
+                        style = MaterialTheme.typography.h5,
+                        color = MaterialTheme.colors.onBackground
+                    )
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Box(
+                        Modifier
+                            .fillMaxWidth()
+                            .height(1.dp)
+                            .background(MaterialTheme.colors.secondary)
+                    )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 30.dp),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Button(onClick = {
+                            // TODO: Enter op id someway
+                            //navController.navigate(Screen.xxxxxxxxx.route)
+                        }) {
+                            Text(
+                                text = "Physical Inventory",
+                                style = MaterialTheme.typography.h5,
+                                color = MaterialTheme.colors.background
+                            )
+                        }
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 50.dp),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Button(onClick = {
+                            // TODO: Enter op id someway
+                            //navController.navigate(Screen.xxxxxxxxx.route)
+                        }) {
+                            Text(
+                                text = "Upload Collected Data",
+                                style = MaterialTheme.typography.h5,
+                                color = MaterialTheme.colors.background
+                            )
+                        }
+                    }
+                }
+            }
+        )
+    }
+}
