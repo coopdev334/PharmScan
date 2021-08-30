12da46c91c448340eb7ade9734613fb33b8aa61e
mike <meehm334@gmail.com>
2021-08-29 22:30:39 -0400

    Added display info for tag and file for NavHostScanScreen

--- app/src/main/java/com/example/pharmscan/ui/Navigation/NavHostScanScreen.kt
+++ app/src/main/java/com/example/pharmscan/ui/Navigation/NavHostScanScreen.kt
@@ -11,6 +11,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.example.pharmscan.ui.Screen.Screen
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.mutableStateOf
@@ -18,6 +19,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavType
 import androidx.navigation.compose.navArgument
@@ -130,7 +133,8 @@ fun NavGraphBuilder.addScanScreen(navController: NavController) {
             },
             content = {
                 Column(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier
+                        .fillMaxSize()
                 ) {
                     Row(
                         modifier = Modifier
@@ -144,6 +148,113 @@ fun NavGraphBuilder.addScanScreen(navController: NavController) {
                             color = MaterialTheme.colors.onBackground
                         )
                     }
+                    Spacer(modifier = Modifier.height(height = 8.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(height = 80.dp)
+                            .clip(RoundedCornerShape(50.dp))
+                            .background(MaterialTheme.colors.secondary)
+                    ){
+                        Column() {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                Text(
+                                    text = "Tag",
+                                    style = MaterialTheme.typography.h5,
+                                    color = MaterialTheme.colors.onBackground
+                                )
+                            }
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceEvenly
+                            ) {
+                                Text(
+                                    text = "Qty: 345",
+                                    style = MaterialTheme.typography.h5,
+                                    color = MaterialTheme.colors.onBackground
+                                )
+                                //Spacer(modifier = Modifier.width(width = 10.dp))
+                                Text(
+                                    text = "Amount: 2345.33",
+                                    style = MaterialTheme.typography.h5,
+                                    color = MaterialTheme.colors.onBackground
+                                )
+                            }
+
+                        }
+
+                    }
+                    Spacer(modifier = Modifier.height(height = 8.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(height = 80.dp)
+                            .clip(RoundedCornerShape(50.dp))
+                            .background(MaterialTheme.colors.secondaryVariant)
+                    ){
+                        Column() {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                Text(
+                                    text = "File",
+                                    style = MaterialTheme.typography.h5,
+                                    color = MaterialTheme.colors.onBackground
+                                )
+                            }
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceEvenly
+                            ) {
+                                Text(
+                                    text = "Rec Count: 4345",
+                                    style = MaterialTheme.typography.h5,
+                                    color = MaterialTheme.colors.onBackground
+                                )
+                            }
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(height = 8.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(height = 80.dp)
+                            .clip(RoundedCornerShape(50.dp))
+                            .background(MaterialTheme.colors.secondaryVariant)
+                    ){
+                        Column() {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                Text(
+                                    text = "??????",
+                                    style = MaterialTheme.typography.h5,
+                                    color = MaterialTheme.colors.onBackground
+                                )
+                            }
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceEvenly
+                            ) {
+                                Text(
+                                    text = "Something: xxxxxxxxxxx",
+                                    style = MaterialTheme.typography.h5,
+                                    color = MaterialTheme.colors.onBackground
+                                )
+                            }
+                        }
+                    }
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
@@ -154,22 +265,27 @@ fun NavGraphBuilder.addScanScreen(navController: NavController) {
                             modifier = Modifier.fillMaxWidth(),
                             horizontalArrangement = Arrangement.SpaceEvenly
                         ){
-                            // TODO: Button just for testing status bar color and text change
-                            // use this to change info on this screen as user selects options
-                            // and status or other displays.
-                            Button(onClick = {
+                            Button(
+                                modifier = Modifier
+                                    .clip(RoundedCornerShape(50.dp))
+                                    .size(width = 110.dp, height = 50.dp),
+                                onClick = {
                                 statusBarBkgrColor = Color.Yellow
                                 argTextstatusBar = "*** Scan Tag ***"
                                 coroutineScope.launch(Dispatchers.Default) {
+                                    // TODO: Implement Changetagscan function
                                     // supend fun ChangeTagScan()
-                                    println("coop change tag start")
                                 }
                             }
                             ) {
                                 Text(text = "Change Tag")
 
                             }
-                            Button(onClick = {
+                            Button(
+                                modifier = Modifier
+                                    .clip(RoundedCornerShape(50.dp))
+                                    .size(width = 110.dp, height = 50.dp),
+                                onClick = {
                                 statusBarBkgrColor = Color.Cyan
                                 argTextstatusBar = "*** Hold ***"
                                 // ChangeTagScan()
@@ -179,7 +295,7 @@ fun NavGraphBuilder.addScanScreen(navController: NavController) {
 
                             }
                         }
-                        Spacer(modifier = Modifier.height(height = 10.dp))
+                        Spacer(modifier = Modifier.height(height = 8.dp))
                         Row(
                             modifier = Modifier.fillMaxWidth(),
                             horizontalArrangement = Arrangement.SpaceEvenly
@@ -187,8 +303,13 @@ fun NavGraphBuilder.addScanScreen(navController: NavController) {
                             // TODO: Button just for testing status bar color and text change
                             // use this to change info on this screen as user selects options
                             // and status or other displays.
-                            Button(onClick = {
-                                  navController.popBackStack(Screen.PhysInvUploadScreen.route, inclusive = false)
+                            Button(
+                                modifier = Modifier
+                                    .clip(RoundedCornerShape(50.dp))
+                                    .size(width = 250.dp, height = 50.dp),
+                                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
+                                onClick = {
+                                  navController.popBackStack()
                             }
                             ) {
                                 Text(text = "Quit")
