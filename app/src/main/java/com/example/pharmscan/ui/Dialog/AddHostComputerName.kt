cee96b4b61563e086b0a53036f3b2c91ffc3a2fb
mike <meehm334@gmail.com>
2021-09-08 21:39:11 -0400

    updgrade cancel coll data record dialog

--- app/src/main/java/com/example/pharmscan/ui/Dialog/AddHostComputerName.kt
+++ app/src/main/java/com/example/pharmscan/ui/Dialog/AddHostComputerName.kt
@@ -1,96 +1,5 @@
 package com.example.pharmscan.ui.Dialog
 
-//import androidx.compose.foundation.layout.*
-//import androidx.compose.material.*
-//import androidx.compose.runtime.Composable
-//import androidx.compose.runtime.*
-//import androidx.compose.runtime.saveable.rememberSaveable
-//import androidx.compose.ui.Alignment
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.unit.dp
-//
-//@Composable
-//fun AddHostComputer(
-//    showDialog: Boolean,
-//    onAdd: (name: String) -> Unit,
-//    onCancel: () -> Unit
-//) {
-//    var text by rememberSaveable { mutableStateOf("") }
-//
-//    if (showDialog) {
-//        AlertDialog(
-//            onDismissRequest = {
-//                onCancel()
-//            },
-//            modifier = Modifier.size(200.dp, 200.dp),
-//            title = {
-//                Column(
-//                    modifier = Modifier.padding(top = 20.dp, bottom = 10.dp)
-//                ) {
-//                    Text(
-//                        text = "Add",
-//                        style = MaterialTheme.typography.body1
-//                    )
-//                }
-//            },
-//            text = {
-//                OutlinedTextField(
-//                    value = text,
-//                    onValueChange = {
-//                        text = it
-//                    },
-//                    label = {
-//                        Column(
-//                            modifier = Modifier.padding(bottom = 20.dp)
-//                        ) {
-//                            Text(
-//                                text = "Host Computer",
-//                                style = MaterialTheme.typography.body1
-//                            )
-//                        }
-//                    },
-//                    singleLine = true,
-//                    textStyle = MaterialTheme.typography.h6
-//                )
-//            },
-//            buttons = {
-//                Column(
-//                    horizontalAlignment = Alignment.End,
-//                    modifier = Modifier
-//                        .padding(all = 8.dp)
-//                        .fillMaxWidth()
-//                ) {
-//                    Spacer(modifier = Modifier.height(35.dp))
-//                    Row(
-//                        verticalAlignment = Alignment.Bottom
-//                    ) {
-//                        TextButton(
-//                            onClick = { onCancel() }
-//                        ) {
-//                            Text(
-//                                text = "Cancel",
-//                                style = MaterialTheme.typography.subtitle1
-//                            )
-//                        }
-//                        Button(
-//                            onClick = {
-//                                if (text.isNotEmpty()) {
-//                                    onAdd(text)
-//                                }
-//                            }
-//                        ) {
-//                            Text(
-//                                text = " Add ",
-//                                style = MaterialTheme.typography.subtitle1
-//                            )
-//                        }
-//                    }
-//                }
-//            }
-//        )
-//    }
-//}
-
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -101,7 +10,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.unit.dp
-import com.example.pharmscan.ui.Utility.*
 
 @Composable
 fun AddHostComputer(
--- app/src/main/java/com/example/pharmscan/ui/Dialog/CancelCollDataRecord.kt
+++ app/src/main/java/com/example/pharmscan/ui/Dialog/CancelCollDataRecord.kt
@@ -1,10 +1,78 @@
 package com.example.pharmscan.ui.Dialog
 
+//import androidx.compose.foundation.layout.*
+//import androidx.compose.material.*
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.unit.dp
+//
+//@Composable
+//fun CancelCollDataRecord(
+//    collDataRec: String,
+//    showDialog: Boolean,
+//    onDismiss: () -> Unit,
+//    onCancelRecord: () -> Unit
+//) {
+//
+//    if (showDialog) {
+//        AlertDialog(
+//            onDismissRequest = {
+//                onDismiss()
+//            },
+//            modifier = Modifier.size(240.dp, 230.dp),
+//            title = {
+//                Text(
+//                    text = "Cancel?",
+//                    style = MaterialTheme.typography.h6
+//                )
+//            },
+//            text = {
+//                Text(
+//                    text = collDataRec,
+//                    style = MaterialTheme.typography.h5
+//                )
+//            },
+//            buttons = {
+//                Column(
+//                    horizontalAlignment = Alignment.End,
+//                    modifier = Modifier
+//                        .padding(all = 8.dp)
+//                        .fillMaxWidth()
+//                ) {
+//                    Spacer(modifier = Modifier.height(35.dp))
+//                    Row(
+//                        verticalAlignment = Alignment.Bottom
+//                    ) {
+//                        TextButton(
+//                            onClick = { onDismiss() }
+//                        ) {
+//                            Text(
+//                                text = "Ignore",
+//                                style = MaterialTheme.typography.subtitle1
+//                            )
+//                        }
+//                        Button(
+//                            onClick = { onCancelRecord() }
+//                        ) {
+//                            Text(
+//                                text = " Cancel ",
+//                                style = MaterialTheme.typography.subtitle1
+//                            )
+//                        }
+//                    }
+//                }
+//            }
+//        )
+//    }
+//}
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -14,50 +82,48 @@ fun CancelCollDataRecord(
     onDismiss: () -> Unit,
     onCancelRecord: () -> Unit
 ) {
+    val requester = FocusRequester()
 
     if (showDialog) {
         AlertDialog(
             onDismissRequest = {
                 onDismiss()
             },
-            modifier = Modifier.size(200.dp, 200.dp),
-            title = {
-                Text(
-                    text = "Cancel?",
-                    style = MaterialTheme.typography.h6
-                )
-            },
-            text = {
-                Text(
-                    text = collDataRec,
-                    style = MaterialTheme.typography.h5
-                )
-            },
+            modifier = Modifier.size(240.dp, 230.dp),
             buttons = {
                 Column(
-                    horizontalAlignment = Alignment.End,
                     modifier = Modifier
                         .padding(all = 8.dp)
-                        .fillMaxWidth()
+                        .fillMaxSize()
                 ) {
-                    Spacer(modifier = Modifier.height(35.dp))
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Text(
+                        text = "Cancel?",
+                        style = MaterialTheme.typography.h4
+                    )
                     Row(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalArrangement = Arrangement.End,
                         verticalAlignment = Alignment.Bottom
                     ) {
                         TextButton(
+                            modifier = Modifier.size(width = 90.dp, height = 48.dp),
                             onClick = { onDismiss() }
                         ) {
                             Text(
                                 text = "Ignore",
-                                style = MaterialTheme.typography.subtitle1
+                                style = MaterialTheme.typography.h5
                             )
                         }
                         Button(
-                            onClick = { onCancelRecord() }
+                            modifier = Modifier.size(width = 100.dp, height = 45.dp),
+                            onClick = {
+                                onCancelRecord()
+                            }
                         ) {
                             Text(
                                 text = " Cancel ",
-                                style = MaterialTheme.typography.subtitle1
+                                style = MaterialTheme.typography.h6
                             )
                         }
                     }
