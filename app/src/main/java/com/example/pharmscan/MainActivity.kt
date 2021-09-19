0a967738cd7bd4156729b2a4134c51cfbf2933c0
Matthew-J-M <mjm227@njit.edu>
2021-09-19 17:55:19 -0400

    Merged scan information with File Handler and merged with Audio Handling.

--- app/src/main/java/com/example/pharmscan/MainActivity.kt
+++ app/src/main/java/com/example/pharmscan/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.pharmscan.Data.PharmScanDb
+import com.example.pharmscan.Data.ScanLiveData
 import com.example.pharmscan.Data.Tables.PSNdc
 import com.example.pharmscan.Repository.PharmScanRepo
 import com.example.pharmscan.ViewModel.PharmScanViewModel
@@ -26,6 +27,8 @@ import com.example.pharmscan.ui.theme.PharmScanTheme
 import java.io.File
 
 class MainActivity() : ComponentActivity() {
+    private lateinit var psViewModel : PharmScanViewModel
+    private lateinit var receiver : PharmScanBroadcastReceiver
 
     // TODO: @ExperimentalFoundationApi just for Text(.combinedClickable) may go away
     @ExperimentalFoundationApi
@@ -36,8 +39,24 @@ class MainActivity() : ComponentActivity() {
         val database = PharmScanDb.getDatabase(this)
         val repo = PharmScanRepo(database.getHostCompNameDao(), database.getCollectedDataDao(), database.getSystemInfoDao(), database.getPSNdcDao(), database.getSettingsDao())
         val factory = PharmScanViewModelFactory(repo)
+        psViewModel = ViewModelProvider(this, factory).get(PharmScanViewModel::class.java)
+
+        receiver = PharmScanBroadcastReceiver(psViewModel)
         val pharmScanViewModel = ViewModelProvider(this, factory).get(PharmScanViewModel::class.java)
 
+        //Create broadcast receiver for scanner
+        val intentFilter = IntentFilter()
+        intentFilter.addAction("com.example.pharmscan.ACTION")
+        intentFilter.addCategory(Intent.CATEGORY_DEFAULT)
+        PharmScanApplication.context?.registerReceiver(receiver, intentFilter)
+
+        // Disable scanner when app starts
+        val intent = Intent()
+        intent.setAction("com.symbol.datawedge.api.ACTION")
+        intent.putExtra("com.symbol.datawedge.api.SCANNER_INPUT_PLUGIN", "DISABLE_PLUGIN")
+        sendBroadcast(intent)
+
+        readFileLineByLineUsingForEachLine(psViewModel, "/sdcard/Download/psndc.dat")
         setContent {
             PharmScanTheme {
                 // A surface container using the 'background' color from the theme
@@ -47,81 +66,46 @@ class MainActivity() : ComponentActivity() {
             }
         }
     }
-    readLines("/sdcard/Download/psndc.dat")
-}
-fun readLines(fileName: String) {
-    checkPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE, 101)
-    File(fileName).readBytes()
-}
-
-// Function to check and request permission.
-fun checkPermission(permission: String, requestCode: Int) {
-    Log.d("TESTING", "Checking the " + permission + " permissions.")
-    if (ContextCompat.checkSelfPermission(
-            this@MainActivity,
-            permission
-        ) == PackageManager.PERMISSION_DENIED
-    ) {
-        // Requesting the permission
-        ActivityCompat.requestPermissions(
-            this@MainActivity,
-            arrayOf(permission),
-            requestCode
-        )
-    } else {
-        Toast.makeText(this@MainActivity, "Permission already granted", Toast.LENGTH_SHORT)
-            .show()
-    }
-}
-override fun onRequestPermissionsResult(requestCode: Int,
-                                        permissions: Array<String>,
-                                        grantResults: IntArray) {
-    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    if (requestCode == 101) {
-
-        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this@MainActivity, "Storage Permission Granted", Toast.LENGTH_SHORT).show()
-            Log.d("TESTING", "Inside permissions result.")
-            File("/sdcard/Download/psndc.dat").readBytes()
-        } else {
-            Toast.makeText(this@MainActivity, "Storage Permission Denied", Toast.LENGTH_SHORT).show()
+    // Reading the data
+    fun readFileLineByLineUsingForEachLine(pharmScanViewModel: PharmScanViewModel, fileName: String){
+        checkPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE, 101)
+        pharmScanViewModel.deleteAllPSNdc()
+        var psndc = PSNdc("","","")
+        File(fileName).forEachLine {
+            psndc.ndc = it.substring(0..10)
+            psndc.price = it.substring(11..16) + "." + it.substring(17..18)
+            psndc.packsz = it.substring(19..26)
+            pharmScanViewModel.insertPSNdc(psndc)
         }
     }
-}
-}
-}
-//Create broadcast receiver for scanner
-val intentFilter = IntentFilter()
-intentFilter.addAction("com.example.pharmscan.ACTION")
-intentFilter.addCategory(Intent.CATEGORY_DEFAULT)
-PharmScanApplication.context?.registerReceiver(receiver, intentFilter)
-
-// Disable scanner when app starts
-val intent = Intent()
-intent.setAction("com.symbol.datawedge.api.ACTION")
-intent.putExtra("com.symbol.datawedge.api.SCANNER_INPUT_PLUGIN", "DISABLE_PLUGIN")
-sendBroadcast(intent)
-
-readFileLineByLineUsingForEachLine(psViewModel, "/sdcard/Download/psndc.dat")
-///sdcard/Download/psndc.dat
-setContent {
-    PharmScanTheme {
-        // A surface container using the 'background' color from the theme
-        //Surface{
-        Navigate(psViewModel)
-        //}
+    // Function to check and request permission.
+    fun checkPermission(permission: String, requestCode: Int) {
+        Log.d("TESTING", "Checking the " + permission + " permissions.")
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                permission
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            // Requesting the permission
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(permission),
+                requestCode
+            )
+        } else {
+            Toast.makeText(this@MainActivity, "Permission already granted", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
-}
-}
 
-override fun onResume() {
-    // Disable scanner when app starts
-    val intent = Intent()
-    intent.setAction("com.symbol.datawedge.api.ACTION")
-    intent.putExtra("com.symbol.datawedge.api.SCANNER_INPUT_PLUGIN", "DISABLE_PLUGIN")
-    sendBroadcast(intent)
-    super.onResume()
-}
+    override fun onResume() {
+        // Disable scanner when app starts
+        val intent = Intent()
+        intent.setAction("com.symbol.datawedge.api.ACTION")
+        intent.putExtra("com.symbol.datawedge.api.SCANNER_INPUT_PLUGIN", "DISABLE_PLUGIN")
+        sendBroadcast(intent)
+        super.onResume()
+    }
 
 //        override fun onRestart() {
 //        // Disable scanner when app starts
@@ -132,14 +116,14 @@ override fun onResume() {
 //        super.onRestart()
 //    }
 
-override fun onPause() {
-    // Disable scanner when app starts
-    val intent = Intent()
-    intent.setAction("com.symbol.datawedge.api.ACTION")
-    intent.putExtra("com.symbol.datawedge.api.SCANNER_INPUT_PLUGIN", "DISABLE_PLUGIN")
-    sendBroadcast(intent)
-    super.onPause()
-}
+    override fun onPause() {
+        // Disable scanner when app starts
+        val intent = Intent()
+        intent.setAction("com.symbol.datawedge.api.ACTION")
+        intent.putExtra("com.symbol.datawedge.api.SCANNER_INPUT_PLUGIN", "DISABLE_PLUGIN")
+        sendBroadcast(intent)
+        super.onPause()
+    }
 
 
 // The code below is for a different method of working with broadcast receivers
@@ -196,17 +180,6 @@ class PharmScanBroadcastReceiver(pharmScanViewModel: PharmScanViewModel) : Broad
             psViewModel.scanLiveData.value = ScanLiveData(decodedData, decodedLabelType)
 
         }
-    }
-
-}
-
-fun readFileLineByLineUsingForEachLine(pharmScanViewModel: PharmScanViewModel, fileName: String){
-    var psndc = PSNdc("","","")
-    File(fileName).forEachLine {
-        psndc.ndc = it.substring(0..10)
-        psndc.price = it.substring(11..18)
-        psndc.packsz = it.substring(19..26)
-        pharmScanViewModel.insertPSNdc(psndc)
     }
 
 }
