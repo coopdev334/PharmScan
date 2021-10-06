da3188a4ae1ba5bbd6b87ce8ee1a3c87b334fdd2
Matthew-J-M <mjm227@njit.edu>
2021-10-06 15:13:29 -0400

    Changed path to work with Zebara device.

--- app/src/main/java/com/example/pharmscan/ui/Utility/Logging.kt
+++ app/src/main/java/com/example/pharmscan/ui/Utility/Logging.kt
@@ -8,7 +8,7 @@ import java.nio.file.StandardOpenOption
 
 public fun writeToFile(data: String, context: Context?) {
     if (context != null) {
-        val path: File? = context!!.getExternalFilesDir(null)
+        val path: String = "/sdcard/Download/"//context!!.getExternalFilesDir(null)
         Log.d("TESTING", path.toString())
         val file: File? = File(path, "PharmaScanLogs.txt")
         var content = data + "\n"
